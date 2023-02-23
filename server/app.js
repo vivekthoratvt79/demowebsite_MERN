0aa0c37f640c1582d728e6110718fff9894cc04e
Vivek <vivekthorat.vt79@gmail.com>
2023-02-23 11:23:08 +0530

    middleware added

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 
+//Middleware
+const middleware = (req, res, next) => {
+  next();
+};
+
 app.get("/", (req, res) => {
   res.send("Hello woorld");
 });
 app.get("/contact", (req, res) => {
   res.send("Coontact Page");
 });
-app.get("/about", (req, res) => {
+app.get("/about", middleware, (req, res) => {
   res.send("About page");
 });
 app.get("/signin", (req, res) => {
@@ -17,5 +22,5 @@ app.get("/signup", (req, res) => {
   res.send("Signup");
 });
 app.listen(3080, () => {
-  console.log(`Server is running at port 3000`);
+  console.log(`Server is running at port 3080`);
 });
